4898b61096ef3d1549791b37d26cb44d6fba637a
Meheedi-Hasan <mahdyhassann@gmail.com>
2022-06-14 21:53:28 +0600

    Added DDT to read data from excel sheet

--- src/test/java/testcase/Registration.java
+++ src/test/java/testcase/Registration.java
@@ -1,10 +1,14 @@
 package testcase;
-
+import java.io.File;
+import java.io.FileInputStream;  
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import java.io.FileNotFoundException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import base.Driver;
 
@@ -22,22 +26,43 @@ public class Registration extends Driver {
 	}
 	
 	@Test
-	public static void registration() throws InterruptedException
-	{
+	public static void registration() throws InterruptedException, Exception{
 		
+		File src = new File("E:\\Riseup Labs\\Eclipse-Workspace\\Data.xlsx");
+		FileInputStream fis = new FileInputStream(src);
+		XSSFWorkbook xsf = new XSSFWorkbook(fis);
+		XSSFSheet sheet = xsf.getSheetAt(0);
+		
+		String u_name = sheet.getRow(0).getCell(0).getStringCellValue();
+		System.out.println(u_name);
+		String email = sheet.getRow(0).getCell(1).getStringCellValue();
+		String pass = sheet.getRow(0).getCell(2).getStringCellValue();
+		String con_pass = sheet.getRow(0).getCell(2).getStringCellValue();
+		String FullName = sheet.getRow(0).getCell(3).getStringCellValue();
+		int phone1 = (int) sheet.getRow(0).getCell(4).getNumericCellValue();
+		String phone = String.valueOf(phone1);
+		
+		int age1 = (int) sheet.getRow(0).getCell(5).getNumericCellValue();
+		String age = String.valueOf(age1);
+		
+		String Inst = sheet.getRow(0).getCell(6).getStringCellValue();
+		
+		xsf.close();
+		
+	
 		driver.findElement(By.xpath("//a[normalize-space()='Register']")).click();
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//span//div[@class='col-md-6']//input[@placeholder='Enter username']")).sendKeys("Ayann");
+		driver.findElement(By.xpath("//span//div[@class='col-md-6']//input[@placeholder='Enter username']")).sendKeys(u_name);
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@placeholder='Enter your E-mail']")).sendKeys("ayan@gmail.com");
+		driver.findElement(By.xpath("//input[@placeholder='Enter your E-mail']")).sendKeys(email);
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//span//div[@class='col-md-6']//input[@placeholder='Enter password']")).sendKeys("123456789");
+		driver.findElement(By.xpath("//span//div[@class='col-md-6']//input[@placeholder='Enter password']")).sendKeys(pass);
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@placeholder='Confirm password']")).sendKeys("123456789");
+		driver.findElement(By.xpath("//input[@placeholder='Confirm password']")).sendKeys(con_pass);
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys("Ayan Islam");
+		driver.findElement(By.xpath("//input[@placeholder='Enter your full name']")).sendKeys(FullName);
 		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@placeholder='Enter your phone number']")).sendKeys("01732838583");
+		driver.findElement(By.xpath("//input[@placeholder='Enter your phone number']")).sendKeys("0"+phone);
 		Thread.sleep(3000);
 		
 		WebElement dropDown = driver.findElement(By.name("Gender"));
@@ -45,7 +70,7 @@ public class Registration extends Driver {
 		se.selectByValue("male");
 		Thread.sleep(3000);
 		
-		driver.findElement(By.xpath("//input[@placeholder='Age']")).sendKeys("25");
+		driver.findElement(By.xpath("//input[@placeholder='Age']")).sendKeys(age);
 		Thread.sleep(3000);
 		
 		WebElement Dg = driver.findElement(By.name("Designation"));
@@ -53,13 +78,18 @@ public class Registration extends Driver {
 		Dg_Se.selectByValue("_others_");
 		Thread.sleep(3000);
 		
-		driver.findElement(By.xpath("//input[@placeholder='Enter institution name']")).sendKeys("RiseupLabs");
+		driver.findElement(By.xpath("//input[@placeholder='Enter institution name']")).sendKeys(Inst);
 		Thread.sleep(3000);
 		
 		driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
 		Thread.sleep(3000);
 		
-		
+	
+	}
+
+	private static FileInputStream FileInputStream(String string) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 	
