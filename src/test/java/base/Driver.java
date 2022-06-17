edf914819101e136fb726ca27e6ed24d9d9aeade
Meheedi-Hasan <mahdyhassann@gmail.com>
2022-06-17 21:31:44 +0600

    Added new login page

--- src/test/java/base/Driver.java
+++ src/test/java/base/Driver.java
@@ -1,29 +1,41 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Driver {
 
+	public static String url = "https://sam.rultest2.com/";
 	public static WebDriver driver = null;
+	public static WebElement element;
 	
     @BeforeSuite
-	
 	public void setup()
 	{
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		
 	}
-	/*
-	@AfterSuite
+    
+    @BeforeClass
+	public static void openPage() throws InterruptedException, Exception{
+		
+		
+		driver.get(url);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+	}
+    
 	
+	@AfterSuite
 	public void tearDown()
 	{
 		driver.quit();
-	} */
+	}
 }
